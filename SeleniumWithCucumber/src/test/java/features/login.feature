7c050dfccd5ee00f5781c4218bf6840675d4290d
Akanksha Tomar <AKANKSHAT4@GMAIL.COM>
2022-03-05 20:32:31 +0530

    working source code for implementation of selenium with cucumber

--- SeleniumWithCucumber/src/test/java/features/login.feature
+++ SeleniumWithCucumber/src/test/java/features/login.feature
@@ -2,7 +2,8 @@ Feature: Login action
   Scenario: Free CRM log in test scenario
     Given User is available on the Login page
     When Title of the login page is Free CRM
-    And User enters userName
-    And User enters password
-    And User clicks on login button
+    When User clicks on login link
+    When User enters userName
+    When User enters password
+    When User clicks on login button
     Then User should be logged in to the free CRM home page
--- SeleniumWithCucumber/src/test/java/runner/TestRunner.java
+++ SeleniumWithCucumber/src/test/java/runner/TestRunner.java
@@ -1,10 +1,16 @@
 package runner;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "src/test/java/stepDefinition")
+@CucumberOptions(features = "C:\\Users\\HP\\Documents\\Study\\Programming\\SeleniumWithCucumber\\src\\test\\java\\features",
+        glue = {"stepDefinition"},
+       plugin = {"testng", "html:target/cucumber-report/cucumber.html",
+        "json:target/cucumber-report/cucumber.json",
+        "junit:target/cucumber-report/cucumber.xml"},
+        monochrome = true, publish = true, stepNotifications = true)
 public class TestRunner {
 }
